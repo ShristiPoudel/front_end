4c7e21108c0e41f8d47de8b11b80fae74e789427
ShristiPoudel <shristypoudel31@gmail.com>
2025-05-18 05:20:47 +0545

    PageNotFound

--- frontend/src/Pages/PageNotFound/PageNotFound.jsx
+++ frontend/src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { use } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './PageNotFound.css'
+
 
 const PageNotFound = () => {
+  let navigate = useNavigate();
+  useEffect(()=>{
+    setTimeout(()=>navigate('/'),3000);
+  },[])
   return (
-    <div>PageNotFound</div>
+    <div className='page-not-found'>Page not Found...You will be redirected in Homepage after 3 seconds...</div>
   )
 }
 
